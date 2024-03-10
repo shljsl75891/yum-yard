54cc5f4a20f775391a962bb1ca61bda30f8fa1a2
shljsl75891 <shljsl75891@gmail.com>
2024-03-10 22:22:02 +0530

    update(components): better ui primitive components

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,12 +1,19 @@
-interface ButtonProps {
-  children: string;
+import { HTMLAttributes, ReactNode } from "react";
+
+interface ButtonProps extends HTMLAttributes<HTMLButtonElement> {
+  disabled?: boolean;
+  children: ReactNode;
 }
 
 const Button: React.FC<ButtonProps> = (props) => {
-  const { children } = props;
+  const { disabled, children, className, ...rest } = props;
 
   return (
-    <button className="px-3 py-1 cursor-pointer rounded-lg bg-red-200">
+    <button
+      className={`px-3 py-1 rounded-lg outline-none ${disabled ? "cursor-auto bg-gray-200" : "drop-shadow-md active:drop-shadow-none " + className}`}
+      disabled={disabled}
+      {...rest}
+    >
       {children}
     </button>
   );
--- src/components/ui/TextInput.tsx
+++ src/components/ui/TextInput.tsx
@@ -3,11 +3,12 @@ interface TextInputProps {}
 const TextInput: React.FC<
   TextInputProps & React.InputHTMLAttributes<HTMLInputElement>
 > = (props) => {
+  const { className, ...rest } = props;
   return (
     <input
       type="text"
-      className="m-1 px-3 py-1 outline-none bg-white rounded-2xl"
-      {...props}
+      className={`m-1 px-3 py-1 outline-none bg-white rounded-2xl ${className}`}
+      {...rest}
     />
   );
 };
