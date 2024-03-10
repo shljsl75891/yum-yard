ea2b256b0c19c884f584f99f06594fc6186cdf3c
shljsl75891 <shljsl75891@gmail.com>
2024-03-10 15:02:39 +0530

    feat(fonts): merriweather fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Merriweather Sans", "ui-sans-serif", "system-ui"],
+        serif: ["Merriweather", "ui-serif", "Georgia"],
+      },
+    },
   },
   plugins: [],
 };
